15bca0a7353835f344eeae98e71067de7ce7c421
thinnathirakesh <109718599+thinnathirakesh@users.noreply.github.com>
2023-01-26 00:01:04 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int su=0;         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         int k=s.nextInt();         for(int i=0;i<k;i++)         {             su+=a[i];         }         System.out.println(su);     } }

---
+++ sum_of_array__elements_upto__Kth__number_in_an_array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int su=0;
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        int k=s.nextInt();
+        for(int i=0;i<k;i++)
+        {
+            su+=a[i];
+        }
+        System.out.println(su);
+    }
+}
